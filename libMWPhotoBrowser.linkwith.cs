da7211e5a1c3ae19051c6b3e54f7641869c40f9a
Mapo80 <mapo80@gmail.com>
2014-03-23 16:23:11 +0100

    Fix Compile SDWebImage

--- libMWPhotoBrowser.linkwith.cs
+++ libMWPhotoBrowser.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libMWPhotoBrowser.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, Frameworks="ImageIO MessageUI Foundation")]
+[assembly: LinkWith ("libMWPhotoBrowser.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, Frameworks="MapKit UIKit CoreGraphics Foundation ImageIO MessageUI")]
